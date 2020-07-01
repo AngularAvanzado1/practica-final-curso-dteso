f45aa0bdc5d89615131d555ce9d8dc3c9b718e85
David Teso <david.teso@ezentis.com>
2020-07-01 20:55:05 +0200

    pwa features - (caché config+update available msg)

--- practica-final/apps/practica-final/src/app/app.component.ts
+++ practica-final/apps/practica-final/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from "../../../../libs/shared/api/src/lib/api/api.service";
+import { SwUpdate, SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'practica-final-root',
@@ -10,11 +11,39 @@ export class AppComponent implements OnInit {
   title = 'CONTINENTS';
   continents;
 
-  constructor(private apiService: ApiService) { }
+  constructor(
+    private apiService: ApiService,
+    private swUpdate: SwUpdate,
+    private swPush: SwPush
+  ) {
+    this.checkVersionUpdates();
+  }
 
   ngOnInit() {
     this.apiService.getContinents$().subscribe(res => {
       this.continents = res;
     });
+  }
+
+  private checkVersionUpdates() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.checkForUpdate().then(data => {
+        console.log('Checking for Updates...');
+      });
+      this.swUpdate.available.subscribe(event => {
+        if (event) {
+          alert(JSON.stringify(event));
+        }
+        if (event.available.appData) {
+          const appData: any = event.available.appData;
+          let msg = `New version ${appData.version} available.`;
+          msg += `${appData.changelog}.`;
+          msg += 'Reaload?';
+          if (confirm(msg)) {
+            window.location.reload();
+          }
+        }
+      });
+    }
   }
 }
