ae6050c6264ef418f1d94b4745ee5051f43a1e64
David Teso <david.teso@ezentis.com>
2020-07-02 17:08:38 +0200

    full service unit test + script

--- practica-final/apps/practica-final-e2e/src/integration/app.spec.ts
+++ practica-final/apps/practica-final-e2e/src/integration/app.spec.ts
@@ -2,12 +2,8 @@ import { getGreeting } from '../support/app.po';
 
 describe('practica-final', () => {
   beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
+  it('should display header defined message', () => {
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to practica-final!');
+    getGreeting().contains("Geograpfics from 'The World Bank' API");
   });
 });
--- practica-final/libs/shared/api/src/lib/api/api.service.spec.ts
+++ practica-final/libs/shared/api/src/lib/api/api.service.spec.ts
@@ -1,16 +1,122 @@
-import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
 import { ApiService } from './api.service';
 
-describe('ApiService', () => {
-  let service: ApiService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
-  });
+describe('GIVEN: a ApiService', () => {
+  describe('WHEN: the DataModule is compiled', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule]
+      });
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+    /*service*/
+    it('THEN: service should be created', () => {
+      const service: ApiService = TestBed.get(ApiService);
+      expect(service).toBeTruthy();
+    });
+
+
+    /*continents*/
+    it(`THEN in CONTINENTS: should return an observable when call 'getContinents$()'`, () => {
+      const service: ApiService = TestBed.get(ApiService);
+      const continents$: Observable<any> = service.getContinents$();
+      expect(continents$).toBeInstanceOf(Observable);
+    });
+    it(`THEN in CONTINENTS: continents list should not be empty when call 'getContinents$()'`, () => {
+      const service: ApiService = TestBed.get(ApiService);
+      const continents$: Observable<any> = service.getContinents$();
+      let data = [];
+      continents$.subscribe(res => {
+        data = res;
+      })
+      expect(data.length > 0).toBeTruthy;
+    });
+
+    it(`THEN in CONTINENTS: should return 'It works' when call 'getContinents()'`,
+      async(() => {
+        const service: ApiService = TestBed.get(ApiService);
+        service
+          .getContinents$()
+          .subscribe(result =>
+            expect(result).toEqual({ message: 'It works' })
+          );
+        const httpMock = TestBed.get(HttpTestingController); // mock del backend para no depender del servidor
+        const req = httpMock.expectOne('http://api.worldbank.org/v2/region/?format=json'); // esperar a que se llame a esta ruta
+        req.flush({ message: 'It works' }); // responder con esto
+        httpMock.verify(); // comprobar que no hay más llmadas
+      }));
+
+
+    /*regions*/
+    it(`THEN in REGIONS: should return an observable when call 'getRegions$(code)'`, () => {
+      const code = 'ECS'
+      const service: ApiService = TestBed.get(ApiService);
+      const regions$: Observable<any> = service.getRegions$(code);
+      expect(regions$).toBeInstanceOf(Observable);
+    });
+    it(`THEN in REGIONS: continents list should not be empty when call 'getRegions$()'`, () => {
+      const code = 'ECS'
+      const service: ApiService = TestBed.get(ApiService);
+      const regions$: Observable<any> = service.getRegions$(code);
+      let data = [];
+      regions$.subscribe(res => {
+        data = res;
+      })
+      expect(data.length > 0).toBeTruthy;
+    });
+
+    it(`THEN in REGIONS: should return 'It works' when call 'getRegions()'`,
+      async(() => {
+        const code = 'ECS'
+        const service: ApiService = TestBed.get(ApiService);
+        service
+          .getRegions$(code)
+          .subscribe(result =>
+            expect(result).toEqual({ message: 'It works' })
+          );
+        const httpMock = TestBed.get(HttpTestingController); // mock del backend (simulación)
+        const req = httpMock.expectOne(`http://api.worldbank.org/v2/region/${code}/country?per_page=1000&format=json`); // la api debe estar operativa aunque simulemos su respuesta
+        req.flush({ message: 'It works' }); // respuesta simulada siempre que se haya obtenido conexión
+        httpMock.verify();
+      }));
+
+    /*country*/
+    it(`THEN in COUNTRY: should return an observable when call 'getCountry(id)'`, () => {
+      const id = 'BHR'
+      const service: ApiService = TestBed.get(ApiService);
+      const country$: Observable<any> = service.getCountry(id);
+      expect(country$).toBeInstanceOf(Observable);
+    });
+    it(`THEN in COUNTRY: response only has an unique value 'getCountry(id)'`, () => {
+      const id = 'BHR'
+      const service: ApiService = TestBed.get(ApiService);
+      const country$: Observable<any> = service.getCountry(id);
+      let data = [];
+      country$.subscribe(res => {
+        data = res;
+      })
+      expect(data.length === 1).toBeTruthy;
+    });
+
+    it(`THEN in country: should return 'It works' when call 'getCountry(id)'`,
+      async(() => {
+        const id = 'BHR'
+        const service: ApiService = TestBed.get(ApiService);
+        service
+          .getCountry(id)
+          .subscribe(result =>
+            expect(result).toEqual({ message: 'It works' })
+          );
+        const httpMock = TestBed.get(HttpTestingController); // mock del backend (simulación)
+        const req = httpMock.expectOne(`http://api.worldbank.org/V2/country/${id}?format=json`); // la api debe estar operativa aunque simulemos su respuesta
+        req.flush({ message: 'It works' }); // respuesta simulada siempre que se haya obtenido conexión
+        httpMock.verify();
+      }));
   });
 });
